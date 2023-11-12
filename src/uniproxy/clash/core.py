871e4cc4a87078b3e270a1400d5358fe007cca2e
Lanqing Huang <lqhuang@outlook.com>
2023-11-12 08:46:12 +0000

    feat: Add `ClashConfig` for `uni-proxy/clash`

---
+++ src/uniproxy/clash/core.py
@@ -0,0 +1,34 @@
+from typing import Iterable, Literal, TypeAlias
+
+from .abc import AbstractClash
+
+Hosts: TypeAlias = dict[str, str]
+Proxies: TypeAlias = Iterable[Proxy]
+ProxyProviders: TypeAlias = list[ProxyProvider]
+ProxyGroups: TypeAlias = list[ProxyGroup]
+RuleProviders: TypeAlias = list[RuleProvider]
+Rules: TypeAlias = list[Rule]
+
+
+class ClashConfig(AbstractClash):
+    mode: Literal["Rule", "Global", "Direct"]
+    log_level: Literal["silent", "info", "warning", "error", "debug"]
+    ipv6: bool
+
+    port: int
+    socks_port: int
+    redir_port: int
+    mixed_port: int
+
+    allow_lan: bool
+    bind_address: str
+    external_controller: str
+
+    dns: Dns
+    tun: Tun
+    hosts: Hosts
+    proxies: Proxies
+    proxy_providers: ProxyProviders
+    proxy_groups: ProxyGroups
+    rule_providers: RuleProviders
+    rules: Rules
