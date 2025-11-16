64654769e1514b90c25a9c216a1f0d005159cdf8
Lanqing Huang <lqhuang@outlook.com>
2025-11-16 18:28:19 +0800

    chore: Remove all Scala codes

--- uniproxy/test/src/rules.scala
+++
@@ -1,8 +0,0 @@
-import uniproxy.rules.Rule
-
-// object RuleTests extends munit.FunSuite:
-
-//   test("hello") {
-//     val rule = Rule("domain", "google.com", "direct")
-//     assertEquals(rule.toString, "Rule(domain,google.com,direct)")
-//   }
--- uniproxy/test/src/singbox/rules.scala
+++
@@ -1,9 +0,0 @@
-import uniproxy.singbox.route.Rule
-
-class RuleTests extends munit.FunSuite:
-
-  test("hello") {
-    val rule = Rule(outbound = "direct", domain = Some("google.com"))
-
-    assertEquals(rule.outbound, "direct")
-  }
