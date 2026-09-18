cb9323d3464175cf7afc3c5ac2fb9a6ea5e1ec46
Lanqing Huang <lqhuang@outlook.com>
2026-09-18 18:05:24 +0800

    refactor: Rename and reorg converters and suites

--- src/uniproxy/cross_converters/clash/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from .rules import clash_rules_from_uniproxy as clash_rules_from_uniproxy
--- src/uniproxy/cross_converters/surge/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from .rules import surge_rules_from_uniproxy as surge_rules_from_uniproxy
